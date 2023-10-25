d76e041dd560274a7516ba6a42f1ce69e9fb038e
Elad Joseph <eladj2002@gmail.com>
2023-10-25 11:26:01 +0300

    tryed to add a way to swith the writing in the game.html file

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from flask import Flask ,render_template,request,redirect
 import json
-
+import random as rd
+usedQustions = []
 app = Flask(__name__,template_folder="template")
-
+with open("quistions.text", "r")as fo:
+    quistions = fo.readlines()
+    numberOfRows = len(fo.readlines())
 with open("users.json","r") as f:
     users = json.load(f)
 
@@ -19,9 +22,18 @@ def quiz():
             users[email] = password
             with open("users.json","w") as f:
                 json.dump(users,f)
-            return render_template("game.html")
+            quistionNumber = rd.randint(0,len(quistions)-1)
+            usedQustions.append(quistionNumber)
+            quistion = quistions[quistionNumber].split(",")
+            print(quistion)
+            return render_template("game.html",
+                                   question = quistion[0],
+                                   answer1 = quistion[1],
+                                   answer2 = quistion[2],
+                                   answer3 = quistion[3],
+                                   answer4 = quistion[4])
         else:
             return render_template("sigh_in.html")
-        
+
 
 app.run()
