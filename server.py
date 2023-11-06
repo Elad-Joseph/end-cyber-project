29c0340af497d7cd2121b6d57a854f669bce6c4e
Elad Joseph <eladj2002@gmail.com>
2023-11-06 18:38:39 +0200

    continue

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask ,render_template,request,redirect
 import json
 import random as rd
 usedQustions = []
+answers = [2,3,1,2,4,1]
+quistionNumber : int
 app = Flask(__name__,template_folder="template")
 with open("quistions.text", "r")as fo:
     quistions = fo.readlines()
@@ -13,6 +15,21 @@ with open("users.json","r") as f:
 def sigh_in():
     return render_template("sigh_in.html")
 
+def setUpPage():
+    # quistionNumber = -1
+    # print(len(quistions))
+    # while quistionNumber not in usedQustions: 
+    #     quistionNumber = rd.randint(0,len(quistions)-1)
+    # quistionNumber = 6
+    # usedQustions.append(quistionNumber)
+    quistion = quistions[6].split(",")
+    return render_template("game.html",
+                            question = quistion[0],
+                            answer1 = quistion[1],
+                            answer2 = quistion[2],
+                            answer3 = quistion[3],
+                            answer4 = quistion[4])
+
 @app.route("/quiz",methods=["GET","POST"])
 def quiz():
     if request.method == "POST":
@@ -22,18 +39,23 @@ def quiz():
             users[email] = password
             with open("users.json","w") as f:
                 json.dump(users,f)
-            quistionNumber = rd.randint(0,len(quistions)-1)
-            usedQustions.append(quistionNumber)
-            quistion = quistions[quistionNumber].split(",")
-            print(quistion)
+                quistion = quistions[7].split(",")
+                print(quistion)
+                quistion[1] = str(quistion[1])
             return render_template("game.html",
-                                   question = quistion[0],
-                                   answer1 = quistion[1],
-                                   answer2 = quistion[2],
-                                   answer3 = quistion[3],
-                                   answer4 = quistion[4])
+                            question = quistion[0],
+                            answer1 = quistion[1],
+                            answer2 = quistion[2],
+                            answer3 = quistion[3],
+                            answer4 = quistion[4])
+            # setUpPage()
         else:
             return render_template("sigh_in.html")
 
+@app.route("/answer1",methods=["GET","POST"])
+def answer1():
+    if 1 == answers[quistionNumber]:
+        setUpPage()
 
 app.run()
+
